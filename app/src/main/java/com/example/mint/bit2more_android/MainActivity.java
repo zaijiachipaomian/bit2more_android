b49ab289e017a5c284ab08f5015c33a7a83b8e18
cseyhsu@gmail.com <cseyhsu@gmail.com>
2018-11-12 15:04:49 +0800

    android练习手册¬

---
+++ app/src/main/java/com/example/mint/bit2more_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mint.bit2more_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// android 练习手册
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
